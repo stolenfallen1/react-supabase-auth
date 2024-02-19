1c40ec5fd8fd607064565406ad01c5147d5d473f
Paw <jquizeo_ccs@uspf.edu.ph>
2024-02-19 21:11:28 +0800

    project init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center">
+                <h1 className="font-bold text-4xl">Hello world</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
