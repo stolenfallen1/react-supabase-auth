bd2b03f1320037c94fff01306878e0679d04d33e
Paw <jquizeo_ccs@uspf.edu.ph>
2024-02-21 21:19:18 +0800

    feature: implemented github login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { FaGoogle, FaFacebookF, FaTwitter } from "react-icons/fa";
+import { FaGoogle, FaFacebookF, FaTwitter, FaGithub } from "react-icons/fa";
 import SPLIT_IMAGE from "../assets/Images/split_screen_img.jpg";
 import { supabase } from "../config/supabaseClient";
 import { useNavigate } from "react-router-dom";
@@ -20,40 +20,61 @@ export default function Login() {
         }
     }
 
-    // TODO: Implement Google Sign In
-    // async function signInWithGoogle() {
-    //     await supabase.auth.signInWithOAuth({
-    //         provider: "google",
-    //     });
-    // }
+    async function signInWithGitHub() {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: "github",
+        });
+        if (error) {
+            console.log("Error signing in with GitHub", error.message);
+        }
+        if (data) {
+            console.log(data);
+            localStorage.setItem("userData", JSON.stringify(data));
+            navigate("/home");
+        }
+    }
+
+    // TODO: Implement signInWithGoogle, signInWithTwitter and signInWithGitHub functions
 
     return (
         <>
             <main className="h-screen flex items-center justify-center">
                 <section className="w-1/2 flex flex-col items-center justify-center">
-                    <form className="max-w-lg border border-black/75 rounded-lg flex flex-col gap-8">
-                        <div className="border-2 mx-4 mt-8 flex items-center justify-center rounded-lg">
-                            <button className="py-4 px-16 flex gap-4 items-center">
-                                <FaGoogle className="text-red-500" />
-                                <span>Sign in with Google</span>
-                            </button>
-                        </div>
-                        <div className="border-2 mx-4 flex items-center justify-center rounded-lg">
-                            <button
-                                onClick={signInWithFacebook}
-                                className="py-4 px-16 flex gap-4 items-center"
-                            >
-                                <FaFacebookF className="text-blue-500" />
-                                <span>Sign in with Facebook</span>
-                            </button>
-                        </div>
-                        <div className="border-2 mx-4 mb-8 flex items-center justify-center rounded-lg">
-                            <button className="py-4 px-16 flex gap-4 items-center">
-                                <FaTwitter className="text-sky-500" />
-                                <span>Sign in with Twitter</span>
-                            </button>
-                        </div>
-                    </form>
+                    <div className="flex flex-col items-center justify-center border-2 border-black/25 rounded-lg">
+                        <button
+                            onClick={signInWithFacebook}
+                            className="py-4 px-16 flex gap-4 items-center"
+                        >
+                            <FaFacebookF className="text-blue-500" />
+                            <span className="border-b-2 border-b-black/25 hover:border-b-black">
+                                Sign in with Facebook
+                            </span>
+                        </button>
+
+                        <button
+                            onClick={signInWithGitHub}
+                            className="py-4 px-16 flex gap-4 items-center"
+                        >
+                            <FaGithub className="text-black/75" />
+                            <span className="border-b-2 border-b-black/25 hover:border-b-black">
+                                Sign in with GitHub
+                            </span>
+                        </button>
+
+                        <button className="py-4 px-16 flex gap-4 items-center">
+                            <FaGoogle className="text-red-500" />
+                            <span className="border-b-2 border-b-black/25 hover:border-b-black">
+                                Sign in with Google
+                            </span>
+                        </button>
+
+                        <button className="py-4 px-16 flex gap-4 items-center">
+                            <FaTwitter className="text-sky-500" />
+                            <span className="border-b-2 border-b-black/25 hover:border-b-black">
+                                Sign in with Twitter
+                            </span>
+                        </button>
+                    </div>
                 </section>
                 <section className="w-1/2">
                     <div className="h-screen flex">
